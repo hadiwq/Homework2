c47a6bb7cde51ade5f957ec45aca48bb58c53c61
hadiwq <98136543+hadiwq@users.noreply.github.com>
2022-09-17 19:27:02 -0500

    Update handlepost.php

--- handlepost.php
+++ handlepost.php
@@ -6,17 +6,7 @@
   <input type="submit">
 </form>
 
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  // collect value of input field
-  $name = $_POST['fname'];
-  if (empty($name)) {
-    echo "Name is empty";
-  } else {
-    echo $name;
-  }
-}
-?>
+
 
 </body>
 </html>
