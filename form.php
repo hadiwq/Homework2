898ace8e374f6b3cd336fb6c3d03102e148aca4a
hadiwq <98136543+hadiwq@users.noreply.github.com>
2022-09-17 19:13:54 -0500

    Create form.php

---
+++ form.php
@@ -0,0 +1,15 @@
+<form method="POST" action="form2.php">
+    <pre>Name: <input type="text"
+        name="user_name">
+    </pre>
+      
+    <pre>Email Address: <input type="text"
+        name="user_email_address">
+    </pre>
+      
+    <pre>Mobile Number: <input type="number"
+        name="user_mobile_number">
+    </pre>
+      
+    <input type="submit" value="Next">
+</form>
