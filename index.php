1d0c1f52ccd0742acb773ebf4a445751a493209f
hadiwq <98136543+hadiwq@users.noreply.github.com>
2022-09-17 19:27:29 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,5 +2,6 @@
 <h1>My website</h1>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
   <?php require_once("form.php"); ?>
+<?php require_once("handlepost.php"); ?>
 </body>
 </html>
