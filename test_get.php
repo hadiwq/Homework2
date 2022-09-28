df095b07c751d3f98aa96bc5c8318198b15dc01d
hadiwq <98136543+hadiwq@users.noreply.github.com>
2022-09-27 19:23:03 -0500

    Update test_get.php

--- test_get.php
+++ test_get.php
@@ -1,23 +1,13 @@
+
+
 <html>
 <body>
 
 <?php 
-<a href="form.php?">Test $GET</a>
-echo "<h2>Your Input:</h2>";
-echo $name;
-echo "<br>";
-echo $email;
-echo "<br>";
-echo $website;
-echo "<br>";
-echo $comment;
-echo "<br>";
-echo $gender;
+echo "Study " . $_GET['name'] . " at " . $_GET['web'];
 ?>
 
 </body>
 </html>
-?>
 
-</body>
-</html>
+
